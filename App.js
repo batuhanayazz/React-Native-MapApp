487cb6ee4ff800943166bcb44503b98447d69a35
batuhanayazz <43605933+batuhanayazz@users.noreply.github.com>
2023-11-25 14:45:54 +0100

    Bug fixed + Creating bottom navBar

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
 import { StyleSheet, Text, View } from "react-native";
+
+import TabNavigation from "./App/Navigations/TabNavigation";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>TEST!</Text>
-      <StatusBar style="auto" />
+      <NavigationContainer>
+        <TabNavigation />
+      </NavigationContainer>
     </View>
   );
 }
@@ -14,7 +18,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
