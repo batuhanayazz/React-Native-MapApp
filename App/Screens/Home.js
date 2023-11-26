8cf83bcbb95a9fa3f19aaaa86b6f28c02a5f9630
Batuhan Ayaz <batuhan.ayaz@student.ehb.be>
2023-11-26 18:40:55 +0100

    Can get Data from Google place api

--- App/Screens/Home.js
+++ App/Screens/Home.js
@@ -1,15 +1,30 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Header from "../Components/Home/Header";
 import GoogleMapView from "../Components/Home/GoogleMapView";
 import CategoryList from "../Components/Home/CategoryList";
+import PlaceList from "../Components/Home/PlaceList";
+import GlobalApi from "../Services/GlobalApi";
 
 export default function Home() {
+  const [placeList, setPlaceList] = useState([]);
+  useEffect(() => {
+    GetNearBySearchPlace();
+  }, []);
+
+  const GetNearBySearchPlace = () => {
+    GlobalApi.nearByPlace().then((resp) => {
+      console.log("resp", resp);
+
+    });
+  };
+
   return (
     <View style={{ padding: 20, marginTop: 30 }}>
       <Header />
       <GoogleMapView />
       <CategoryList />
+      {placeList ? <PlaceList placeList={placeList} /> : null}
     </View>
   );
 }
