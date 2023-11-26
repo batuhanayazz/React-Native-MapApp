8bb83f8ae1c955921baec2b458af509f9c79041b
Batuhan Ayaz <batuhan.ayaz@student.ehb.be>
2023-11-26 17:29:52 +0100

    Google map User Location Working + bug fixing

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import * as Location from "expo-location";
 
 import { useFonts } from "expo-font";
 
 import TabNavigation from "./App/Navigations/TabNavigation";
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
   return (
     <View style={styles.container}>
       <NavigationContainer>
--- App/Components/Home/GoogleMapView.js
+++ App/Components/Home/GoogleMapView.js
@@ -1,16 +1,18 @@
 import { View, Text, Dimensions } from "react-native";
 import React from "react";
-import MapView from "react-native-maps";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
 export default function GoogleMapView() {
   return (
-    <View>
+    <View style={{ marginTop: 20, borderRadius: 20, overflow: "hidden" }}>
       <MapView
         style={{
           width: Dimensions.get("screen").width * 0.89,
           height: Dimensions.get("screen").height * 0.23,
           borderRadius: 20,
         }}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation={true}
       ></MapView>
     </View>
   );
